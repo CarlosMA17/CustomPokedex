1ecf8483b8cdb007677a4da6d43ed6d5ed6e81c1
Carlos Martel <102035692+CarlosMA17@users.noreply.github.com>
2023-04-14 12:59:25 +0100

    feat(PokemonLegendario): se ha aaÃñadido un constructor a la clase y se ha añadido el metodo toString

--- src/pokemons/PokemonLegendario.java
+++ src/pokemons/PokemonLegendario.java
@@ -3,4 +3,26 @@ package pokemons;
 public class PokemonLegendario extends Pokemon{
     private String localizacion;
 
+    public PokemonLegendario(String nombre, String tipo, float altura, float peso, String descripcion, String localizacion) {
+        super(nombre, tipo, altura, peso, descripcion);
+        this.localizacion = localizacion;
+    }
+
+    public String getLocalizacion() {
+        return localizacion;
+    }
+
+
+    @Override
+    public String toString() {
+        return  "|---------------------------|\n|\t\t\t\t" +
+                this.getNombre() + "\t\t|\n" +
+                "|\t\t\t\t" + this.getTipo() + "\t\t|\n" +
+                "|\t\t\t\tHT\t" + this.getAltura() + "\t\t|\n" +
+                "|\t\t\t\tWT\t" + this.getPeso() + "\t\t|\n" +
+                "|\t\t\t\t" + this.getLocalizacion() + "\t|\n" +
+                "|-[]-[]-[]-[]---[]-[]-[]-[]-|\n" +
+                this.getDescripcion() +
+                "\n|---------------------------|";
+    }
 }
