82c062b5aa0551b7995b6859ae37bfa2a28d2333
Carlos Martel <102035692+CarlosMA17@users.noreply.github.com>
2023-04-21 00:50:21 +0100

    fix(listaPokemon): se ha recortado codigo no util de la funcion addPokemon

--- src/Utiles/ListaPokemon.java
+++ src/Utiles/ListaPokemon.java
@@ -11,9 +11,6 @@ public class ListaPokemon {
 
     public void addPokemon(Pokemon pokemon) {
         Pokemon[] listaPokemonNueva = new Pokemon[listaPokemon.length + 1];
-        for (int i = 0; i < listaPokemon.length; i++) {
-            listaPokemonNueva[i] = listaPokemon[i];
-        }
         for (int i = 0; i < listaPokemonNueva.length; i++) {
             if (listaPokemonNueva[i] == null) {
                 listaPokemonNueva[i] = pokemon;
